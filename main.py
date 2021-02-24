6ef4f107653f4080e2079644700fc3ddf6006c66
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2021-02-23 22:39:23 -0600

    Achieved CSV creation and sending as a response

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from os import getenv
 from typing import List
 
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, Response, UploadFile
 from fastapi.responses import HTMLResponse
 
+from src.csv_creation import create_csv_file
 from src.xml_processing import create_csv_row_from_xml
 
 port = getenv("PORT", 5000)
@@ -14,7 +15,8 @@ app = FastAPI()
 @app.post("/upload-xmls/")
 async def process_xmls(xml_files: List[UploadFile] = File(...)):
     processed_files = [create_csv_row_from_xml(xml_file) for xml_file in xml_files]
-    return processed_files
+    csv_content = create_csv_file(processed_files)
+    return Response(content=csv_content, media_type="text/csv")
 
 
 @app.get("/")
---
+++ src/csv_creation.py
@@ -0,0 +1,27 @@
+import csv
+from tempfile import NamedTemporaryFile
+
+
+def create_csv_file(csv_rows: list[dict[str, str]]) -> str:
+    temp_file = NamedTemporaryFile()
+
+    with open(temp_file.name, "w") as writer_file:
+        fieldnames = [
+            "Nombre",
+            "Rfc",
+            "SubTotal",
+            "Fecha",
+            "Folio",
+            "Concepto 1",
+            "Concepto 2",
+            "Concepto 3",
+        ]
+        writer = csv.DictWriter(writer_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for row_dict in csv_rows:
+            writer.writerow(row_dict)
+
+    with open(temp_file.name) as reader_file:
+        return reader_file.read()
