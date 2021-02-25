557a6d9065a198d5cdffcce53dd8b77b02a7a4ea
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2021-02-24 20:00:51 -0600

    Finished adding file list to the end

--- static/main.js
+++ static/main.js
@@ -1,6 +1,23 @@
+const updateFileListUi = (filenames) => {
+  const fileList = document.getElementById("file-list");
+
+  filenames.forEach((filename) => {
+    const itemNode = document.createElement("li");
+    const textNode = document.createTextNode(filename.substring(0, 6) + "...");
+    itemNode.appendChild(textNode);
+    fileList.appendChild(itemNode);
+  });
+};
+
+const updateInstructionsButton = (filenames) => {
+  const button = document.getElementById("instructions-button");
+  button.textContent = `${filenames.length} archivos seleccionados`;
+};
+
 const fileInput = document.getElementById("file-input");
 
 fileInput.addEventListener("change", (event) => {
   const fileNames = Array.from(event.target.files).map((file) => file.name);
-  console.log(fileNames);
+  updateFileListUi(fileNames);
+  updateInstructionsButton(fileNames);
 });
